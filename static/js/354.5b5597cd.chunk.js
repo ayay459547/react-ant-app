2916e9eb88eb3ba9500520d75bcf9b3802fb7a43
ayay459547 <ayay459547@gmail.com>
2023-02-04 22:30:57 +0800

    Updates

---
+++ static/js/354.5b5597cd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_ant_app=self.webpackChunkreact_ant_app||[]).push([[354],{6354:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var r=e(5671),u=e(3144),a=e(136),c=e(7277),i=e(2791),p=e(184),f=function(n){(0,a.Z)(e,n);var t=(0,c.Z)(e);function e(){return(0,r.Z)(this,e),t.apply(this,arguments)}return(0,u.Z)(e,[{key:"render",value:function(){return(0,p.jsx)("div",{children:"Notice"})}}]),e}(i.Component)}}]);
+//# sourceMappingURL=354.5b5597cd.chunk.js.map
---
+++ static/js/574.b4c82fde.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_ant_app=self.webpackChunkreact_ant_app||[]).push([[574],{5574:function(e,t,a){a.r(t);a(2791);var r=a(8935),s=a(7309),n=a(7689),i=a(184);t.default=function(){var e=(0,n.s0)();return(0,i.jsx)("div",{style:{width:"100%",height:"100%"},className:"flex-center",children:(0,i.jsx)(r.ZP,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:(0,i.jsx)(s.ZP,{type:"primary",onClick:function(){return e("/dashboard")},children:"Back Home"})})})}}}]);
+//# sourceMappingURL=574.b4c82fde.chunk.js.map
